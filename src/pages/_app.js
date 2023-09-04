5d19ab19fd6a36f8a2719aadd04db3c56a4b4309
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-09-04 08:38:45 +0700

    build: install react toastify and config _app.js
    This commit overwrite commit ad34deff

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <ToastContainer position="top-right" />
+  </>;
 }
