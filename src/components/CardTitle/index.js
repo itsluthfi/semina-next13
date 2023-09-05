b4fffb6dd5c8f9cf5821735469355f56d2fc00ac
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-09-05 08:51:33 +0700

    feat(components): create card title and card event components

---
+++ src/components/CardTitle/index.js
@@ -0,0 +1,10 @@
+export default function CardTitle({ title, subTitle }) {
+  return (
+    <>
+      <div className="sub-title mb-1" id="grow-today">
+        <span className="text-gradient-pink">{subTitle}</span>
+      </div>
+      <div className="title">{title}</div>
+    </>
+  );
+}
